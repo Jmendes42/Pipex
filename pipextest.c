2b1fd7e62a67ce5bf5328d5bdb4b72d782641c0e
joao-mendes723 <joao.brandao.mendes@gmail.com>
2021-07-29 04:45:27 +0100

    abombarPipes

--- pipextest.c
+++ pipextest.c
@@ -168,21 +168,54 @@ int	command(int argc, char *argv, char *envp[])
 		execve(str3, arg, NULL); 
 		index++;
 	}
+	exit(0);
 	free(arg);
 	return(0);
 }
 
 int 	main(int argc, char *argv[], char *envp[])
 {
-	//int fd[2];
+	int fd[2];
+	int fdi;
 	int pid1;
+	int pid2;
+	int test;
  
+	if (argc != 5)
+		return(0);
+	if (pipe(fd) == -1)
+		printf("ERROR");
 	pid1 = fork(); 
 	if (pid1 < 0)
-		return(0);
-	if (pid1 > 0)	
+		printf("ERROR");
+	if (pid1 == 0)	
+	{
+		test = open(argv[1], O_RDONLY);
+		if (test < 0)
+			return (0);
+		dup2(fd[1], STDOUT_FILENO);
+		dup2(test, 0);
+		close(fd[0]);
+		close(fd[1]);
 		command(argc, argv[2], envp);
+		//printf("%d\n", test);
+	}
+	pid2 = fork(); 
+	if (pid2 < 0)
+		return(0);
+	if (pid2 == 0)
+	{
+		fdi = open( argv[4], O_WRONLY | O_TRUNC);
+		dup2(fdi, 1);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		command(argc, argv[3], envp);
+	}
+	close(fd[0]);
+	close(fd[1]);
 	waitpid(pid1, NULL, 0);
-	command(argc, argv[3], envp);
+	waitpid(pid2, NULL, 0);
+	printf("DONE\n");
 }
-//experimentar por o nr maximo de args para fazer o parsing
+//esta a funcionar corretamente, so as quando tento enviar para o infile e que da erro, nomeadamente o primeiro comando nao funciona
